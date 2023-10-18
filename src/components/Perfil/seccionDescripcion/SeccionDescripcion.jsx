fd40147303c54b5433f42dd290d34958563ff5c3
Ksenia Jarjury <100300372+KseniaJarjury@users.noreply.github.com>
2023-10-18 17:25:03 -0300

    Actualizacion seccionGaleria

--- src/components/Perfil/seccionDescripcion/SeccionDescripcion.jsx
+++ src/components/Perfil/seccionDescripcion/SeccionDescripcion.jsx
@@ -5,28 +5,28 @@ function SeccionDescripcion() {
     return (
         <>
             <div className="contenedor-descrip">
-                <div class=" items-center h-20 ml-12 mt-4 mb-12">
-                    <div class="bg-[#00B0E4] bg-opacity-20 rounded-full inline-block py-2 px-4">
-                        <h3 class="subtitulo text-left">Descripcion: </h3>
+                <div className=" items-center h-20 ml-12 mt-4 mb-12">
+                    <div className="bg-[#00B0E4] bg-opacity-20 rounded-full inline-block py-2 px-4">
+                        <h3 className="subtitulo text-left">Descripcion: </h3>
                     </div>
-                    <div class="contenedor-descrip-perfil">
+                    <div className="contenedor-descrip-perfil">
                         <div>
-                            <p class="descrip-perfil">
+                            <p className="descrip-perfil">
                             Soy un electricista con experiencia dedicado a brindar soluciones eléctricas seguras y eficientes para satisfacer las necesidades de mis clientes. Mi objetivo es garantizar que sus instalaciones eléctricas funcionen de manera óptima y cumplan con los más altos estándares de calidad y seguridad.
                             </p>
                         </div>
                         <div>
-                            <ul class="descrip-info">
+                            <ul className="descrip-info">
                                 <li>
-                                    <img src={circulo} alt="IconoCirculo" class="icon-circulo" />
+                                    <img src={circulo} alt="IconoCirculo" className="icon-circulo" />
                                     <p>Reparacion de toma corrientes domiciliario.</p>
                                 </li>
                                 <li>
-                                    <img src={circulo} alt="IconoCirculo" class="icon-circulo" />
+                                    <img src={circulo} alt="IconoCirculo" className="icon-circulo" />
                                     <p>Armado de llaves termicas Industriales</p>
                                 </li>
                                 <li>
-                                    <img src={circulo} alt="IconoCirculo" class="icon-circulo" />
+                                    <img src={circulo} alt="IconoCirculo" className="icon-circulo" />
                                     <p>Confeccion de planos de obra para la solicitud del servicio.</p>
                                 </li>
                             </ul>
