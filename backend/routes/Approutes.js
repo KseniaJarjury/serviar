40c7162ffdfe1fbf0c476c1b1eddc5dc6178cf50
Maria Belen Ursino <belu.ursino@icloud.com>
2023-11-10 20:46:46 -0300

    Cambios Funcionalidad Foto de Perfil

--- backend/routes/Approutes.js
+++ backend/routes/Approutes.js
@@ -1,12 +1,7 @@
 import express from 'express';
-<<<<<<< Updated upstream
-import { createUsuario, deleteUsuario, filterUsuario, getAllUsuario, getImg, getUsuario, updateUsuario } from '../controllers/UsuarioController.js';
-import { getAllServicio, getServicio, createServicio} from '../controllers/ServicioControllers.js';
-=======
 import multer from 'multer';
 import { createUsuario, deleteUsuario, filterUsuario, getAllUsuario, getUsuario, updateUsuario, cargarImagenPerfil } from '../controllers/UsuarioController.js';
 import { getAllServicio, getServicio, createServicio, updateServicio, deleteServicio} from '../controllers/ServicioControllers.js';
->>>>>>> Stashed changes
 import { getAllProvincia, getProvincia} from '../controllers/ProvinciaControllers.js';
 import { getAllLocalidad } from '../controllers/LocalidadControllers.js';
 
@@ -18,13 +13,10 @@ const upload = multer({ storage: storage });
 router.get('/usuario', getAllUsuario);
 router.get('/usuario/:Id_Usuario', getUsuario);
 router.get('/usuario/:Id_Servicio/:Id_Localidad', filterUsuario);
-<<<<<<< Updated upstream
 router.get('/imagen/:Id_Usuario', getImg);
 router.post('/usuario', createUsuario);
-=======
 router.put('/imagen/:Id_Usuario', cargarImagenPerfil);
 router.post('/registrar', createUsuario);
->>>>>>> Stashed changes
 router.put('/usuario/:Id_Usuario', updateUsuario);
 router.delete('/usuario/:Id_Usuario', deleteUsuario);
 
