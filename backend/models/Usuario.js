84ef92e72308f556b94b06c08f84e0f43ac066a4
Maria Belen Ursino <belu.ursino@icloud.com>
2023-11-10 16:22:39 -0300

    Funcionalidad: Cargar Foto de Perfil

--- backend/models/Usuario.js
+++ backend/models/Usuario.js
@@ -2,6 +2,15 @@
 import db from "../database/db.js";
 //Importamos sequelize
 import { DataTypes } from "sequelize";
+const express = require('express');
+const multer = require('multer');
+
+const app = express();
+
+
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
+
 
 const Usuario = db.define('Usuario',{
     Id_Usuario: {
@@ -23,4 +32,30 @@ const Usuario = db.define('Usuario',{
     tableName: 'usuario'
 });
 
+
+app.post('/cargar-imagen', upload.single('image'), async (req, res) => {
+    const imageBuffer = req.file.buffer;
+  
+    try {
+      // Guarda la imagen en la tabla Usuario
+      const usuarioId = req.body.usuarioId; // Asegúrate de pasar el ID del usuario desde el cliente
+      const usuario = await Usuario.findByPk(usuarioId);
+  
+      if (!usuario) {
+        return res.status(404).json({ message: 'Usuario no encontrado' });
+      }
+  
+      usuario.Foto_Perfil = imageBuffer;
+      await usuario.save();
+      console.log('Imagen guardada en la base de datos');
+      res.status(201).json({ message: 'Imagen de perfil actualizada con éxito' });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: 'Error al cargar la imagen en la base de datos' });
+    }
+  });
+  
+  
 export default Usuario;
+
+
--- frontend/src/components/Oferente/seccionFoto/SeccionFoto.jsx
+++ frontend/src/components/Oferente/seccionFoto/SeccionFoto.jsx
@@ -1,27 +1,57 @@
-import React from "react"
+import React, { useState } from "react";
+import axios from "axios"; // Importa la biblioteca axios
 import FondoPerfil from "../../../assets/fondoPerfil.png";
 import FotoPerfil from "../../../assets/fotoPerfil.png";
 import Start from "../../../assets/start.png";
 import Start1 from "../../../assets/start1.png";
-import "./seccionFoto.css"
+import "./seccionFoto.css";
 
 function SeccionPerfil() {
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  const handleImageUpload = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      setSelectedImage(URL.createObjectURL(file));
+      
+       // Crea un objeto FormData para enviar la imagen al servidor
+       const formData = new FormData();
+       formData.append("image", file);
+ 
+       // Realiza una solicitud POST para cargar la imagen al servidor
+       axios.post("/cargar-imagen", formData)
+         .then((response) => {
+           // Maneja la respuesta del servidor (por ejemplo, muestra un mensaje de éxito)
+           console.log("Imagen cargada con éxito:", response.data);
+         })
+         .catch((error) => {
+           // Maneja errores (por ejemplo, muestra un mensaje de error)
+           console.error("Error al cargar la imagen:", error);
+         });
+
+    }
+  }
+
   return (
     <>
       <div className="cont-foto">
         <div className="contenedor-perfil">
           <img src={FondoPerfil} alt="" className="fondo-perfil" />
           <div className="contenedor-foto-perfil">
-            <img src={FotoPerfil} alt="" className="foto-perfil" />
+            <img src={selectedImage || FotoPerfil} alt="" className="foto-perfil" />
+            <input
+            type="file"
+            accept="image/*"
+            onChange={handleImageUpload}
+            style={{ display: "none" }}
+            id="fileInput"
+          />
+          
           </div>
-          <div className="contenedor-info">
-            <p>Reputación: </p>
-            <img src={Start} alt="" />
-            <img src={Start} alt="" />
-            <img src={Start} alt="" />
-            <img src={Start} alt="" />
-            <img src={Start1} alt="" />
-          </div>
+          <label htmlFor="fileInput" className="upload-button">
+            Cambiar Foto
+          </label>
+
           <div className="contenedor-nombre">
             <div className="nombre-header">
               <h3>Martin Romero</h3>
