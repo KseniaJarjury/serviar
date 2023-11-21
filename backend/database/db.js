aa94d4addeaa4871b08a7e044691b8801329b7ad
Ksenia Jarjury <100300372+KseniaJarjury@users.noreply.github.com>
2023-11-21 18:53:19 -0300

    Update db.js

--- backend/database/db.js
+++ backend/database/db.js
@@ -4,13 +4,13 @@ import dotenv from "dotenv";
 // Cargar variables de entorno desde el archivo .env
 dotenv.config();
 
-const { DB_HOST, DB_PORT, DB_DATABASE, DB_USER, DB_PASSWORD, DB_CONNECTION } = process.env;
+const { DB_HOST, DB_PORT, DB_DATABASE, DB_USER, DB_PASSWORD,  DB_DIALECT } = process.env;
 
 // Crear conexión a la base de datos
 const db = new Sequelize(DB_DATABASE, DB_USER, DB_PASSWORD, {
     host: DB_HOST,
     port: DB_PORT,
-    dialect: DB_CONNECTION,
+    dialect: DB_DIALECT,
 });
 
 export default db;
