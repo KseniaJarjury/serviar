bb98b790d4bcfcfb35a55c05a663c598637a43f9
Ksenia Jarjury <100300372+KseniaJarjury@users.noreply.github.com>
2023-11-21 14:55:48 -0300

    delete frontend

--- frontend/src/hooks/UseServicio.js
+++
@@ -1,8 +0,0 @@
-import { useContext } from "react";
-import ServicioContext from "../context/ServicioProvider";
-
-const UseServicio = () => {
-    return useContext(ServicioContext)
-}
-
-export default UseServicio;
