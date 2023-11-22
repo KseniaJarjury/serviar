b8b3132bf62f227798db761d2fe334a4aecb3781
Ksenia Jarjury <100300372+KseniaJarjury@users.noreply.github.com>
2023-11-22 13:41:44 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api':
-      { 
-        target: 'https://serviar-production.up.railway.app:3000', // Cambia esto a la URL del servidor de Node.js del backend
+      '/api': {
+        target: 'https://serviar-production.up.railway.app', // Cambia esto a la URL del servidor de Node.js del backend
         changeOrigin: true,
-        secure: false,
-        ws: true,
-      }
+        secure: true, // Ajusta a true para habilitar la verificación SSL en producción
+        ws: true, // Habilita si tu backend utiliza WebSocket
+      },
     },
   },
-})
+});
