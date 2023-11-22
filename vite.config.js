cfb00410be2f39f67cc78377373bccfab245d8f0
Ksenia Jarjury <100300372+KseniaJarjury@users.noreply.github.com>
2023-11-22 13:07:15 -0300

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -6,8 +6,11 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': 'https://serviar-production.up.railway.app', // Cambia esto a la URL del servidor de Node.js del backend
+      '/api':
+      { 
+        target: 'https://serviar-production.up.railway.app', // Cambia esto a la URL del servidor de Node.js del backend
+        changeOrigin: true
+      }
     },
-    
   },
 })
