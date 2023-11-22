8e511347dcd305fdab586787702d3fb08bae80a9
Ksenia Jarjury <100300372+KseniaJarjury@users.noreply.github.com>
2023-11-21 21:38:19 -0300

    Update SeccionComentario.jsx

--- frontend/src/components/Inicio/seccionComentario/SeccionComentario.jsx
+++ frontend/src/components/Inicio/seccionComentario/SeccionComentario.jsx
@@ -1,12 +1,12 @@
 import SliderComentarios from "./swiperComentarios/SliderComentarios"
-import "SeccionComentario.css"
+
 
 export default function SeccionComentario() {
     return (
       <>
         <div className="flex justify-center items-center h-40">
           <div className="bg-[#00B0E4] bg-opacity-20 rounded-full inline-block py-2 px-4">
-            <h3 className="sub_comentario text-center">Comentarios</h3>
+            <h3 className="text-black text-[30px] font-bold text-center">Comentarios</h3>
           </div>
         </div>
         <div>
