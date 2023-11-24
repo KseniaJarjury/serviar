2e8582237df48232a4cffecdfb601ec985ae5674
Ksenia Jarjury <100300372+KseniaJarjury@users.noreply.github.com>
2023-11-24 16:24:28 -0300

    Agrego vercel.json

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,9 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    proxy: {
-      '/api': {
-        target: 'https://serviar-production.up.railway.app', // Cambia esto a la URL del servidor de Node.js del backend
-        changeOrigin: true,
-        secure: false, // Ajusta a true para habilitar la verificación SSL en producción
-        ws: true, // Habilita si tu backend utiliza WebSocket
+    server: {
+      proxy: {
+        '/api': 'https://serviar-production.up.railway.app',
       },
     },
   },
