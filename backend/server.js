7b555853e7036b740cd02f4f11a1b19a50e369e2
Ksenia Jarjury <100300372+KseniaJarjury@users.noreply.github.com>
2023-11-25 00:31:18 -0300

    Revert "Revert "Revert "Revert "Merge remote-tracking branch 'origin/backend'""""
    This reverts commit fefa736f61ab959e67ce8be125d441f2be85288d.

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,14 @@ import db from "./database/db.js"
 //Importamos nuestro enrutador
 import AppRoutes from './routes/Approutes.js';
 const app = express();
-app.use(cors())
+const PORT = process.env.PORT || 3000;
+
+app.use(cors({
+    origin: 'https://serviar-git-main-ksenia-jarjurys-projects.vercel.app',  // Reemplaza con tu dominio frontend
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type'],
+}));
+
 app.use(express.json())
 app.use('/api', AppRoutes);
 
@@ -16,6 +23,6 @@ try {
     console.log(`El error de conexion es: ${error}`)
 }
 
-app.listen(3000, () => {
-    console.log('Server UP running in http://localhost:3000/')
+app.listen(PORT, () => {
+    console.log(`'Server UP running in: ${PORT}`)
 })
