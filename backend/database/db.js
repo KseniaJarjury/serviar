660eba412a6b5bb33cf346b39ddf5029e9923abb
Ksenia Jarjury <100300372+KseniaJarjury@users.noreply.github.com>
2023-11-25 00:31:36 -0300

    Revert "Revert "Revert "Revert "Revert "Merge remote-tracking branch 'origin/backend'"""""
    This reverts commit 7b555853e7036b740cd02f4f11a1b19a50e369e2.

--- backend/database/db.js
+++ backend/database/db.js
@@ -1,16 +1,15 @@
 import { Sequelize } from "sequelize";
-import dotenv from "dotenv";
 
-// Cargar variables de entorno desde el archivo .env
-dotenv.config();
 
-const { DB_HOST, DB_PORT, DB_DATABASE, DB_USER, DB_PASSWORD,  DB_DIALECT } = process.env;
-
-// Crear conexión a la base de datos
-const db = new Sequelize(DB_DATABASE, DB_USER, DB_PASSWORD, {
-    host: DB_HOST,
-    port: DB_PORT,
-    dialect: DB_DIALECT,
+const db = new Sequelize ('serviar', 'root', '',{
+    host: 'localhost',
+    dialect: 'mysql',
+    
 });
 
+<<<<<<< HEAD
 export default db;
+=======
+
+export default db
+>>>>>>> parent of 75467c46 (Merge remote-tracking branch 'origin/backend')
