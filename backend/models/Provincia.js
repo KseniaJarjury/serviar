2918b0135965b744aba1baf7bcc0f4dd8160256e
Ksenia Jarjury <100300372+KseniaJarjury@users.noreply.github.com>
2023-11-25 00:30:33 -0300

    Revert "Revert "Merge remote-tracking branch 'origin/backend'""
    This reverts commit d19ab44e6b148671278e15789b2898babbdf7208.

---
+++ backend/models/Provincia.js
@@ -0,0 +1,19 @@
+//Importamos la conexion a la DB
+import db from "../database/db.js";
+//Importamos sequelize
+import { DataTypes } from "sequelize";
+
+const Provincia = db.define('Provincia',{
+    Id_Provincia: {
+        type: DataTypes.INTEGER,
+        primaryKey: true, // Esto define la columna Id_Usuario como clave primaria
+    },
+    Provincia: {type: DataTypes.STRING},
+    LatitudP: {type: DataTypes.DECIMAL},
+    LongitudP: {type: DataTypes.DECIMAL},
+}, {
+    timestamps: false,
+    tableName: 'Provincia'
+});
+
+export default Provincia;
